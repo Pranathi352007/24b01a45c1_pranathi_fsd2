35bd80914094f2fc72a8a3b54c7106555518d665
Pranathi352007 <pranathivadlapudi5@gmail.com>
2026-08-10 18:01:38 +0530

    Delete staticclass.ts

--- staticclass.ts
+++
@@ -1,9 +0,0 @@
-class College {
-    static collegeName: string = "SVECW";
-
-    static displayCollege(): void {
-        console.log("College:", College.collegeName);
-    }
-}
-
-College.displayCollege();
