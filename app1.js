20ec924f2b5bb969ba89d59e25f09db9d7ae4305
Pranathi352007 <pranathivadlapudi5@gmail.com>
2026-09-19 09:51:18 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <h2>HTML Rendering in React</h2>
+
+      <h3 style={{ color: "blue" }}>
+        Welcome to React
+      </h3>
+
+      <p>This HTML content is rendered using JSX.</p>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ app3_1.js
@@ -0,0 +1,6 @@
+import Q3ComponentNesting from "./app3.js/Q3ComponentNesting";
+
+function App() {
+  return <Q3ComponentNesting />;
+}
+export default App;
