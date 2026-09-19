dde20db5667dcf8e4f7e039ae20f05e2545e2191
Pranathi352007 <pranathivadlapudi5@gmail.com>
2026-09-19 10:36:58 +0530

    Add files via upload

---
+++ app5_1.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App5 from "./app5/App5";
+
+function App() {
+  return <App5 />;
+}
+
+export default App;
---
+++ app6_1.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App6 from "./app6.js/App6";
+
+function App() {
+  return <App6 />;
+}
+
+export default App;
